fa093f97c4e791f66a470acc4541ccb79f6f57cc
Douglas <dcvweb123@gmail.com>
2024-01-10 14:02:55 -0800

    feat: added converted to json model

--- backend/src/app/api/predict/model.ts
+++ backend/src/app/api/predict/model.ts
@@ -5,8 +5,9 @@ export async function predict(image: File) {
 	console.log(image);
 
 	const model_path = './model.h5'
-	const handler = tfn.io.fileSystem("./path/to/your/model.json");
-	const model = await tf.loadLayersModel(handler);
-	console.log(model);
+	// const handler = tfn.io.fileSystem("./path/to/your/model.json");
+	// const model = await tf.loadLayersModel(handler);
+	// console.log(model);
+	// tf.loadLayersModel()
 	return 1;
 }
