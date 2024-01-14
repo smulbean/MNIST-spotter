9b5a97f3bce92e7d691002c8c5e98d7c7204151d
douglasichen <dcvweb123@gmail.com>
2024-01-13 17:36:56 -0800

    loaded model

--- backend/src/app/api/predict/model.ts
+++ backend/src/app/api/predict/model.ts
@@ -4,7 +4,9 @@ import * as tf from "@tensorflow/tfjs";
 export async function predict(image: File) {
 	console.log(image);
 
-	const model_path = './model.h5'
+	const model_path = 'converted/model/model.json';
+	const model = await tf.loadLayersModel(model_path);
+	// model.predict(tf.tensor([]))
 	// const handler = tfn.io.fileSystem("./path/to/your/model.json");
 	// const model = await tf.loadLayersModel(handler);
 	// console.log(model);
