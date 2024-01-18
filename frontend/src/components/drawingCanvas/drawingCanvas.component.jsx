3868ae8e1161ab85dd49ac212b9c5631744beddc
douglasichen <dcvweb123@gmail.com>
2024-01-17 21:07:44 -0800

    feat: implemented total client side tfjs

--- frontend/src/components/drawingCanvas/drawingCanvas.component.jsx
+++ frontend/src/components/drawingCanvas/drawingCanvas.component.jsx
@@ -2,18 +2,27 @@ import { useRef, useEffect, useState } from 'react';
 import axios from 'axios';
 import * as tf from "@tensorflow/tfjs"
 
+
+// set model
+const modelURL = `${process.env.REACT_APP_URL}/converted/model/model.json`;
+console.log(modelURL)
+const model = await tf.loadLayersModel(modelURL)
+const modelInputShape = [28, 28];
+
 const DrawingCanvas = () => {
 
 	const canvasRef = useRef(null)
   const contextRef = useRef(null)
   const [isDrawing, setIsDrawing] = useState(false)
 	const [response, setResponse] = useState(null)
-	const [model, setModel] = useState(null)
   
   const borderThickness = 1;
   const canvas2Window = 0.5;
-  const lineWidth = 30;
+  const lineWidth = 50;
+  
 
+
+  
   useEffect(() => {
     const canvas = canvasRef.current;
     canvas.width = Math.min(window.innerWidth, window.innerHeight)*2*canvas2Window;
@@ -32,8 +41,7 @@ const DrawingCanvas = () => {
     context.fill();
     contextRef.current = context;
 
-	// set model
-	const myModel = tf.load
+	
   }, [])
   const startDrawing = ({nativeEvent}) => {
     const {offsetX, offsetY} = nativeEvent;
@@ -73,23 +81,39 @@ const DrawingCanvas = () => {
 		
 		// gray scale + resize
 		var imageTensor = tf.browser.fromPixels(imageData, 3);
-		imageTensor = tf.image.resizeBilinear(imageTensor, [28, 28]);
+		imageTensor = tf.image.resizeBilinear(imageTensor, modelInputShape);
 		imageTensor = tf.image.rgbToGrayscale(imageTensor);
 		// imageTensor.print()
 
 		// set to: 1 - a/255 (0-1 domain and inversion)
 		var buffer = tf.buffer(imageTensor.shape, imageTensor.dtype, imageTensor.dataSync());
-		var newBuffer = tf.buffer([28, 28], imageTensor.dtype)
-		for (let y=0; y<28; y++) {
-			for (let x=0; x<28; x++) {
-				newBuffer.set(1-buffer.get(y, x, 0)/255.0, y, x);
+		var newBuffer = tf.buffer([1, modelInputShape[0], modelInputShape[1]], imageTensor.dtype)
+		for (let y=0; y<modelInputShape[0]; y++) {
+			for (let x=0; x<modelInputShape[1]; x++) {
+				newBuffer.set(1-buffer.get(y, x, 0)/255.0, 0, y, x);
 			}
 		}
+		console.log(newBuffer);
 		imageTensor = newBuffer.toTensor();
 		// imageTensor.print();
 
 		// feed into network
-		var res = 
+		var res = model.predict(imageTensor)
+		res = tf.buffer([1, 10], res.dtype, res.dataSync());
+
+		const argMax = (b) => {
+			let bestV = b.get(0, 0);
+			let bestI = 0;
+			for (let i=1; i<b.shape[1]; i++) {
+				if (b.get(0, i)>bestV) {
+					bestV = b.get(0, i);
+					bestI = i;
+				}
+			}
+			return bestI;
+		}
+
+		await setResponse(argMax(res));
   	};
 
   return (
