eb305707538fb5e21b0f02d0cdcdcbca38b4cdee
GraceY <graceyin0728@gmail.com>
2024-02-04 11:29:26 -0500

    + project description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import './App.css';
 import UploadImage from './components/uploadImage/uploadImage.component';
 import DrawingCanvas from './components/drawingCanvas/drawingCanvas.component';
 
+
+
+
 function App() {
+  
   return (
     <div className="App">
       <br></br>
@@ -12,6 +16,17 @@ function App() {
       {/* <UploadImage /> */}
       <DrawingCanvas />
       <br></br>
+      <section class="glowing-cards">
+        <div class="card">
+          <div class="card-content">
+          <h2>OUR PROJECT</h2>
+          <p>Our project is based on MNIST model, python, 
+              react, flask, and other stuff, blah blah blah,
+              This is an image recognition, feel free to play with the model
+          </p>
+          </div>
+        </div>
+      </section>
       <br></br>
       <footer class="footer">
         <p> &copy;2023 All Rights Reserved</p>
